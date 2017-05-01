d20fd11ab098f840af75e3b888d26c8eb1207c70
David Waite <dwaite498@gmail.com>
2017-05-01 03:14:24 +0000

    WIP hamming assignment.

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,5 +1,23 @@
 class Hamming
-    def compute(strand1, strand2)
-        
+    def self.compute(s, t)
+        result = nil
+        if s == t
+            result = 0
+        elsif s.length == 1
+            result = 1
+        else
+          s_split = s.split(//)
+          t_split = t.split(//)
+          dna = s_split.length
+          s_split.count.times do |i|
+            counter = 0
+            if i == t_split[i]
+                counter += 1
+                puts t_split[i]
+            end
+            puts counter
+          end
+            
+        end
     end
 end
